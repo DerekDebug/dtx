f169e2323ee94a090bec791ece48de4e8316b997
chengxingyuan <yuluoxinsheng@gmail.com>
2019-10-03 15:14:55 +0800

    远程调用处理类，基于httpclient实现

--- provider-server/src/main/java/com/example/providerserver/dtx/remote/RemoteServiceInvokeHandler.java
+++ provider-server/src/main/java/com/example/providerserver/dtx/remote/RemoteServiceInvokeHandler.java
@@ -1,3 +1,28 @@
+package com.example.providerserver.dtx.remote;
+
+import com.alibaba.fastjson.JSONArray;
+
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RemoteServiceInvokeHandler implements InvocationHandler {
     protected Logger log = LoggerFactory.getLogger(this.getClass());
     private Class portType;
@@ -19,7 +44,7 @@ public class RemoteServiceInvokeHandler implements InvocationHandler {
                         CloseableHttpClient httpclient = HttpClientBuilder.create().disableCookieManagement().disableAuthCaching().disableAutomaticRetries().setConnectionManager(pool).build();
                         httpClientMap.put(serviceFileName, httpclient);
                     } catch (Exception e) {
-                        throw new WebException("Init http client error", e);
+                        log.error("Init http client error : "+e.toString());
                     }
                 }
             }
@@ -28,7 +53,7 @@ public class RemoteServiceInvokeHandler implements InvocationHandler {
     }
 
     private String getRemoteUrl() {
-        return "http://" + ConfigHelper.getProp(serviceFileName).get("serviceHost") + "/remote";
+        return "http://" + ConfigHelper.getProp(serviceFileName);
     }
 
     public RemoteServiceInvokeHandler(Class portType) {
@@ -45,14 +70,14 @@ public class RemoteServiceInvokeHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        WebContext ctx = WebContext.get();
+
         if ("toString".equals(method.getName())) {
             return proxy;
         }
 
         //调用远程接口
         HttpPost postMethod = new HttpPost(getRemoteUrl());
-        ByteArrayEntity entry = new ByteArrayEntity(SerializeUtils.hessianSerialize(new ProxyRequest(portType.getName(), method.getName(), args, method.getParameterTypes(), ctx)));
+        ByteArrayEntity entry = new ByteArrayEntity(SerializeUtils.hessianSerialize(new ProxyRequest(portType.getName(), method.getName(), args, method.getParameterTypes())));
         postMethod.setEntity(entry);
         CloseableHttpResponse response = null;
         HttpEntity entity = null;
@@ -67,16 +92,14 @@ public class RemoteServiceInvokeHandler implements InvocationHandler {
                 ProxyResponse r = (ProxyResponse) SerializeUtils.hessianDeserialize(entity.getContent());
                 if (r.isSuccess()) {
                     JSONArray logs = r.getLogs();
-                    if (logs != null && WebContext.get() != null)
-                        WebContext.get().getDatas().addAll(logs);
                     return r.getData();
                 } else {
-                    throw new WebException(StringUtils.isBlank(r.getErrorMsg()) ? "调用远程业务接口时，未知错误" : r.getErrorMsg());
+                    throw new Exception(StringUtils.isBlank(r.getErrorMsg()) ? "调用远程业务接口时，未知错误" : r.getErrorMsg());
                 }
             } else if (num == 404) {
-                throw new RemoteInvokeException("业务服务器地址不存在，当前地址" + getRemoteUrl());
+                throw new Exception("业务服务器地址不存在，当前地址" + getRemoteUrl());
             } else {
-                throw new RemoteInvokeException("请求业务服务器失败，返回" + num);
+                throw new Exception("请求业务服务器失败，返回" + num);
             }
         } catch (Exception e) {
             log.error("Execute remote method error", e);
